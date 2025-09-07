72746d6083fcf3ff781609b5fcce46b7291eaa54
neuecc <ils@neue.cc>
2025-09-07 21:37:03 +0900

    sample telemetry

--- sandbox/GeneratorSandbox/Program.cs
+++ sandbox/GeneratorSandbox/Program.cs
@@ -1,84 +1,109 @@
 ﻿using ConsoleAppFramework;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using System.Diagnostics;
 
-args = ["hello"];
+// Use SigNoz profiling: (view: http://localhost:8080/ )
+// git clone https://github.com/SigNoz/signoz.git
+// cd signoz/deploy/docker
+// docker compose up
+Environment.SetEnvironmentVariable("OTEL_EXPORTER_OTLP_ENDPOINT", "http://localhost:4317"); // 4317 or 4318
 
-var builder = ConsoleApp.Create();
+var builder = Host.CreateApplicationBuilder(args);
 
-builder.UseFilter<NopFilter1>();
-builder.UseFilter<NopFilter2>();
-
-builder.Add<MyClass>();
-
-await builder.RunAsync(args);
-
-[ConsoleAppFilter<NopFilter3>]
-[ConsoleAppFilter<NopFilter4>]
-public class MyClass
+builder.Logging.AddOpenTelemetry(logging =>
 {
-    [ConsoleAppFilter<NopFilter5>]
-    [ConsoleAppFilter<NopFilter6>]
-    public void Hello()
+    logging.IncludeFormattedMessage = true;
+    logging.IncludeScopes = true;
+});
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource =>
     {
-        Console.Write("abcde");
-    }
+        resource.AddService("ConsoleAppFramework Telemetry Sample");
+    })
+    .WithMetrics(metrics =>
+    {
+        metrics.AddRuntimeInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddOtlpExporter();
+    })
+    .WithTracing(tracing =>
+    {
+        tracing.SetSampler(new AlwaysOnSampler())
+            .AddHttpClientInstrumentation()
+            .AddSource(ConsoleAppFrameworkSampleActivitySource.Name)
+            .AddOtlpExporter();
+    })
+    .WithLogging(logging =>
+    {
+        logging.AddOtlpExporter();
+    });
+
+var app = builder.ToConsoleAppBuilder();
+
+app.UseFilter<CommandTracingFilter>();
+
+app.Add("", async ([FromServices] ILogger<Program> logger) =>
+{
+    using var httpClient = new HttpClient();
+    var ms = await httpClient.GetStringAsync("https://www.microsoft.com");
+    logger.LogInformation(ms);
+    var google = await httpClient.GetStringAsync("https://www.google.com");
+    logger.LogInformation(google);
+
+    var ms2 = httpClient.GetStringAsync("https://www.microsoft.com");
+    var google2 = httpClient.GetStringAsync("https://www.google.com");
+    var apple2 = httpClient.GetStringAsync("https://www.apple.com");
+    await Task.WhenAll(ms2, google2, apple2);
+
+    logger.LogInformation(apple2.Result);
+
+    logger.LogInformation("OK");
+});
+
+await app.RunAsync(args);
+
+public static class ConsoleAppFrameworkSampleActivitySource
+{
+    public const string Name = "ConsoleAppFrameworkSample";
+
+    public static ActivitySource Instance { get; } = new ActivitySource(Name);
 }
 
-internal class NopFilter1(ConsoleAppFilter next)
-    : ConsoleAppFilter(next)
+// Sample Activity filter
+internal class CommandTracingFilter(ConsoleAppFilter next) : ConsoleAppFilter(next)
 {
-    public override Task InvokeAsync(ConsoleAppContext context, CancellationToken cancellationToken)
+    public override async Task InvokeAsync(ConsoleAppContext context, CancellationToken cancellationToken)
     {
-        Console.Write(1);
-        return Next.InvokeAsync(context, cancellationToken);
-    }
-}
+        using var activity = ConsoleAppFrameworkSampleActivitySource.Instance.StartActivity("CommandStart");
+        if (activity != null)
+        {
+            activity.SetTag("console_app.command_name", context.CommandName);
+            activity.AddBaggage("console_app.command_args", string.Join(" ", context.EscapedArguments));
+        }
 
-internal class NopFilter2(ConsoleAppFilter next)
-    : ConsoleAppFilter(next)
-{
-    public override Task InvokeAsync(ConsoleAppContext context, CancellationToken cancellationToken)
-    {
-        Console.Write(2);
-        return Next.InvokeAsync(context, cancellationToken);
-    }
-}
-
-internal class NopFilter3(ConsoleAppFilter next)
-    : ConsoleAppFilter(next)
-{
-    public override Task InvokeAsync(ConsoleAppContext context, CancellationToken cancellationToken)
-    {
-        Console.Write(3);
-        return Next.InvokeAsync(context, cancellationToken);
-    }
-}
-
-internal class NopFilter4(ConsoleAppFilter next)
-    : ConsoleAppFilter(next)
-{
-    public override Task InvokeAsync(ConsoleAppContext context, CancellationToken cancellationToken)
-    {
-        Console.Write(4);
-        return Next.InvokeAsync(context, cancellationToken);
-    }
-}
-
-internal class NopFilter5(ConsoleAppFilter next)
-    : ConsoleAppFilter(next)
-{
-    public override Task InvokeAsync(ConsoleAppContext context, CancellationToken cancellationToken)
-    {
-        Console.Write(5);
-        return Next.InvokeAsync(context, cancellationToken);
-    }
-}
-
-internal class NopFilter6(ConsoleAppFilter next)
-    : ConsoleAppFilter(next)
-{
-    public override Task InvokeAsync(ConsoleAppContext context, CancellationToken cancellationToken)
-    {
-        Console.Write(6);
-        return Next.InvokeAsync(context, cancellationToken);
+        try
+        {
+            await Next.InvokeAsync(context, cancellationToken);
+            if (activity != null)
+            {
+                activity.SetStatus(ActivityStatusCode.Ok);
+            }
+        }
+        catch (Exception ex)
+        {
+            if (activity != null)
+            {
+                activity.AddException(ex);
+                activity.SetStatus(ActivityStatusCode.Error);
+            }
+            throw;
+        }
     }
 }
