9055cdb00478228e4e9946b9bd9784317df57bd7
neuecc <ils@neue.cc>
2025-10-23 16:32:57 +0900

    trying to make GlobalOptions(WIP)

--- sandbox/GeneratorSandbox/Program.cs
+++ sandbox/GeneratorSandbox/Program.cs
@@ -1,5 +1,9 @@
 ﻿using ConsoleAppFramework;
 using GeneratorSandbox;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
 
 // fail
 //await ConsoleApp.RunAsync(args, Commands.Save);
@@ -7,12 +11,33 @@ using GeneratorSandbox;
 
 var app = ConsoleApp.Create();
 
+// parse immediately
+var verbose = app.AddOptionalGlobalOptions<bool>(ref args, "-v|--verbose");
+var noColor = app.AddOptionalGlobalOptions<bool>(ref args, "--no-color", "Don't colorize output.");
+var prefixOutput = app.AddRequiredGlobalOptions<string>(ref args, "--prefix-output", "Prefix output with level.");
+var dryRun = app.AddOptionalGlobalOptions<bool>(ref args, "--dry-run");
 
-app.Run(args, true, true, true);
+app.ConfigureServices(x =>
+{
+    // to use command body
+    x.AddSingleton<GlobalOptions>(new GlobalOptions(verbose, noColor, prefixOutput, dryRun));
 
+    // variable for setup other DI
+    x.AddLogging(l =>
+    {
+        var console = l.AddSimpleConsole();
+        if (verbose)
+        {
+            console.SetMinimumLevel(LogLevel.Trace);
+        }
+    });
+});
 
-// fail
-// await ConsoleApp.RunAsync(args, async () => await Task.Delay(1000, CancellationToken.None));
+app.Add<Commands>("");
+
+app.Run(args);
+
+record GlobalOptions(bool Verbose, bool NoColor, string PrefixOutput, bool DryRun);
 
 
 public class Commands
@@ -20,7 +45,7 @@ public class Commands
     /// <summary>
     /// Some sort of save command.
     /// </summary>
-    public async Task<int> Save(CancellationToken ct)
+    public async Task<int> Save(int x, int y)
     {
 
         await Task.Delay(1000);
@@ -30,3 +55,46 @@ public class Commands
 
 
 // `using var posixSignalHandler = PosixSignalHandler.Register(Timeout);`
+
+namespace ConsoleAppFramework
+{
+    internal static partial class ConsoleApp
+    {
+        internal partial class ConsoleAppBuilder
+        {
+            public T AddRequiredGlobalOptions<T>(ref string[] args, [ConstantExpected] string name, [ConstantExpected] string description = "")
+                where T : IParsable<T>
+            {
+                var aliasCount = name.AsSpan().Count("|") + 1;
+                if (aliasCount == 1)
+                {
+                    // TryParse...
+                }
+                else
+                {
+                    string[] aliases = name.Split('|', StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).ToArray();
+
+
+                }
+
+                //for (int i = 0; i < args.Length; i++)
+                //{
+                //    if (args[i] == name)
+                //    {
+                //        args.AsSpan().Count("|");
+                //    }
+                //}
+
+
+
+                return default(T);
+            }
+
+            public T AddOptionalGlobalOptions<T>(ref string[] args, string name, string description = "", T defaultValue = default(T))
+                where T : IParsable<T>
+            {
+                return default(T);
+            }
+        }
+    }
+}
