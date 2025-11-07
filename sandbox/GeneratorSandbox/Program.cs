1b9df49d29b3b4ed60c538a54b3070e86d35a9b5
neuecc <ils@neue.cc>
2025-11-07 17:11:54 +0900

    add DoubleDashEscape test

--- sandbox/GeneratorSandbox/Program.cs
+++ sandbox/GeneratorSandbox/Program.cs
@@ -1,6 +1,37 @@
 ﻿using ConsoleAppFramework;
 
 
-ConsoleApp.Run(args, (int x, int y, ConsoleAppContext context) =>
+args = "some-command hello --global-flag flag-value -- more args here".Split(" ");
+
+var app = ConsoleApp.Create();
+
+app.ConfigureGlobalOptions((ref ConsoleApp.GlobalOptionsBuilder builder) =>
 {
+    var flag = builder.AddGlobalOption<string>("--global-flag");
+    return new GlobalOptions(flag);
 });
+
+app.UseFilter<SomeFilter>();
+app.Add<Commands>();
+app.Run(args);
+
+internal record GlobalOptions(string Flag);
+
+internal class Commands
+{
+    [Command("some-command")]
+    public void SomeCommand([Argument] string commandArg, ConsoleAppContext context)
+    {
+        Console.WriteLine($"ARG: {commandArg}");
+        Console.WriteLine($"ESCAPED: {string.Join(", ", context.EscapedArguments.ToArray()!)}");
+    }
+}
+
+internal class SomeFilter(ConsoleAppFilter next) : ConsoleAppFilter(next)
+{
+    public override async Task InvokeAsync(ConsoleAppContext context, CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"FLAG: {((GlobalOptions)context.GlobalOptions!).Flag}");
+        await Next.InvokeAsync(context, cancellationToken);
+    }
+}
--- tests/ConsoleAppFramework.GeneratorTests/GlobalOptionTest.cs
+++ tests/ConsoleAppFramework.GeneratorTests/GlobalOptionTest.cs
@@ -150,4 +150,48 @@ app.Add("", (int x, int y, ConsoleAppContext context) =>
 app.Run(args);
 """, "--x 10 --dry-run --y 20", "(1000, True) -> (10, 20)");
     }
+
+    [Fact]
+    public void DoubleDashEscape()
+    {
+        verifier.Execute("""
+var app = ConsoleApp.Create();
+
+app.ConfigureGlobalOptions((ref ConsoleApp.GlobalOptionsBuilder builder) =>
+{
+    var flag = builder.AddGlobalOption<string>("--global-flag");
+    return new GlobalOptions(flag);
+});
+
+app.UseFilter<SomeFilter>();
+app.Add<Commands>();
+app.Run(args);
+
+internal record GlobalOptions(string Flag);
+
+internal class Commands
+{
+    [Command("some-command")]
+    public void SomeCommand([Argument] string commandArg, ConsoleAppContext context)
+    {
+        Console.WriteLine($"ARG: {commandArg}");
+        Console.WriteLine($"ESCAPED: {string.Join(", ", context.EscapedArguments.ToArray()!)}");
+    }
+}
+
+internal class SomeFilter(ConsoleAppFilter next) : ConsoleAppFilter(next)
+{
+    public override async Task InvokeAsync(ConsoleAppContext context, CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"FLAG: {((GlobalOptions)context.GlobalOptions!).Flag}");
+        await Next.InvokeAsync(context, cancellationToken);
+    }
+}
+""", "some-command hello --global-flag flag-value -- more args here", """
+FLAG: flag-value
+ARG: hello
+ESCAPED: more, args, here
+
+""");
+    }
 }
